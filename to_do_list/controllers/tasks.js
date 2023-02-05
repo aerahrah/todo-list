16134d7c180846db3f0b4b03ea7b98fd6f6c8618
aerahrah <61181505+aerahrah@users.noreply.github.com>
2023-02-05 18:04:09 +0800

    Implement getAllTask and createTask function

--- to_do_list/controllers/tasks.js
+++ to_do_list/controllers/tasks.js
@@ -1,18 +1,31 @@
-const getAllTask = (req, res)=>{
-    res.send("all items")
-    res.status(200).json({ tasks })
-}
-const createTask = (req, res) =>{
-    res.status(201).json({ tasks })
+const Task = require('../models/Task')
+
+
+const getAllTask = async(req, res) =>{
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({ tasks })
+    } catch (error) {
+        console.log(error)
+    }
+  }
+const createTask = async(req, res) =>{
+    // res.status(201).json({ tasks })
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({ task })
+    } catch (error) {
+        console.log(error)
+    }
 }
 const getSingleTask = (req, res) =>{
-    res.status(200).json({ tasks })
+    // res.status(200).json({ tasks })
 }
 const deleteTask = (req, res) =>{
-    res.status(200).json({ tasks })
+    // res.status(200).json({ tasks })
 }
 const updateTask = (req, res) =>{
-    res.status(200).json({ tasks })
+    // res.status(200).json({ tasks })
 }
 
 module.exports = {
