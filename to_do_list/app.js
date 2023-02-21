a84cf5cb0e9b63d2c3394610c1db5fd468f14df3
Marc Ivan Guillermo <61181505+aerahrah@users.noreply.github.com>
2023-02-21 13:57:53 +0800

    Added env config

--- to_do_list/app.js
+++ to_do_list/app.js
@@ -3,17 +3,17 @@ const app = express();
 const connectionDB = require('./db/connect');
 const tasks = require('./routes/Tasks');
 require('./db/connect');
+require('dotenv').config()
 
 app.use(express.json());
 app.use(express.static('./public'))
 app.use('/api/v1/tasks', tasks);
 const port = 3500
 
-const connectionString = "mongodb+srv://Aerarah:Janedizon40@cluster0.2a76ezk.mongodb.net/?retryWrites=true&w=majority"
 
 const start = async()=>{
     try {
-        await connectionDB(connectionString);
+        await connectionDB(process.env.MONGO_URI);
         app.listen(port, ()=>{
             console.log(`Server is listening to port ${port}....`)
         });
